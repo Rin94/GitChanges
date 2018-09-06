3d88cc28641b2da5a121c348ee2abe3303318e59
Rin94 <jerad.salinas94@gmail.com>
2018-09-06 14:09:46 -0500

    Funcionalidad de calculadora definida

---
+++ mathematics.py
@@ -0,0 +1,19 @@
+
+
+class Calculadora:
+
+
+    def __init__(self, n1,n2):
+        self.num1=n1
+        self.num2=n2
+
+
+    def sumar(self):
+         return self.num1+ self.num2
+
+    def restar(self, num1,num2):
+        return self.num1 - self.num2
+
+
+
+
